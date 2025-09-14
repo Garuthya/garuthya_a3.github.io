4e487c63459b3d4146e8250c0e1cdd0ae87a4434
Garuthya <garuthya@gmail.com>
2025-09-14 11:38:40 +0530

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,10 @@
+document.querySelector('h1').addEventListener('click',showMessage);
+Fancybox.bind("[data-fancybox]");
+$('button').click(filterItems);
+function filterItems(){
+    let button = this;
+    let selector = $( button ).data( 'menu' );
+    console.log( selector );
+    $( '#products img' ).not( selector ).hide(500);
+    $( selector ).show(500);
+}
